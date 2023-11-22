6cbc96dd73f0cfd7f6b3e11b265aeb29ca207ffe
Kevin44France <kevin.sen@themama.ai>
2023-11-22 23:51:27 +0100

    css

--- webApp/app.py
+++ webApp/app.py
@@ -11,20 +11,18 @@ headers = {
     'x-rapidapi-key': "5ffebd22e0msh81794727dc35776p116ef2jsn0264b6e23bd5",
 }
 
-random_joke = "food/jokes/random"
 find = "recipes/findByIngredients"
 randomFind = "recipes/random"
 
 
 @app.route('/')
 def search_page():
-    joke_response = str(requests.request("GET", url + random_joke, headers=headers).json()['text'])
-    return render_template('search.html', joke=joke_response)
+    return render_template('search.html')
 
 
 @app.route('/recipes')
 def get_recipes():
-    if (str(request.args['ingridients']).strip() != ""):
+    if str(request.args['ingridients']).strip() != "":
         # If there is a list of ingridients -> list
         querystring = {"number": "5", "ranking": "1", "ignorePantry": "false",
                        "ingredients": request.args['ingridients']}
@@ -42,15 +40,15 @@ def get_recipes():
 def get_recipe():
     recipe_id = request.args['id']
     recipe_info_endpoint = "recipes/{0}/information".format(recipe_id)
-    ingedientsWidget = "recipes/{0}/ingredientWidget".format(recipe_id)
-    equipmentWidget = "recipes/{0}/equipmentWidget".format(recipe_id)
+    ingedientsWidget = "recipes/{0}/ingredientWidget.json".format(recipe_id)
+    equipmentWidget = "recipes/{0}/equipmentWidget.json".format(recipe_id)
 
     recipe_info = requests.request("GET", url + recipe_info_endpoint, headers=headers).json()
 
     recipe_headers = {
         'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
         'x-rapidapi-key': "5ffebd22e0msh81794727dc35776p116ef2jsn0264b6e23bd5",
-        'accept': "text/html"
+        'accept': "application/json"
     }
     querystring = {"defaultCss": "true", "showBacklink": "false"}
 
@@ -58,8 +56,17 @@ def get_recipe():
                                                         params=querystring).text
     recipe_info['equipmentWidget'] = requests.request("GET", url + equipmentWidget, headers=recipe_headers,
                                                       params=querystring).text
+    print("Ingredient Widget URL:", url + ingedientsWidget)
+    print("Ingredient Widget Headers:", recipe_headers)
+    print("Ingredient Widget Querystring:", querystring)
+
+    # Make the request
+    ingredient_widget_response = requests.request("GET", url + ingedientsWidget, headers=recipe_headers,
+                                                  params=querystring)
+    print("Ingredient Widget Response:", ingredient_widget_response.text)
 
     return render_template('recipe.html', recipe=recipe_info)
+
 
 if __name__ == '__main__':
     app.run()
