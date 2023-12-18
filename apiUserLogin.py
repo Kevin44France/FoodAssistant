fa6663d9edfbccb335cced9b4ae747f73038230a
bellieba <baptiste.bellier@reseau.eseo.fr>
2023-12-18 15:55:36 +0100

    headers global pour 150 requests

---
+++ apiUserLogin.py
@@ -0,0 +1,14 @@
+headersBaptiste = {
+	"X-RapidAPI-Key": "4b05be929fmsh12659f6326e91e3p1a057bjsn8983d6d6aa85",
+	"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+}
+
+headersKevin = {
+	"X-RapidAPI-Key": "5ffebd22e0msh81794727dc35776p116ef2jsn0264b6e23bd5",
+	"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+}
+
+headersMaxime = {
+    "X-RapidAPI-Key": "78b1c06843msh9b59f48a3b08d8cp18e59cjsn174c71b0b2f3",
+    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+}
